1db3ee192dddd02652fcc0dd3477620c4ac80fa6
Brent Yi <brentyi@berkeley.edu>
2019-03-06 14:26:16 -0600

    blue_env

--- meta_mb/envs/blue/real_blue_env.py
+++ meta_mb/envs/blue/real_blue_env.py
@@ -3,18 +3,19 @@ from meta_mb.logger import logger
 import gym
 from gym.envs.mujoco.mujoco_env import MujocoEnv
 from meta_mb.meta_envs.base import MetaEnv
+from blue_interface.blue_interface import BlueInterface
 import os
 
 
-class BlueReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
-    def __init__(self):
+class BlueReacherEnv(MetaEnv, BlueInterface, gym.utils.EzPickle):
+    def __init__(self, side, ip, port=9090):
         self.goal = np.ones((3,))
-        MujocoEnv.__init__(self, os.path.join(os.path.dirname(__file__), "assets", "blue_right_v2.xml"), 2)
+        super(BlueEnv, self).__init__(side, ip, port)
         gym.utils.EzPickle.__init__(self)
 
     def step(self, action):
         self.do_simulation(action, self.frame_skip)
-        vec = self.get_body_com("right_l_finger_tip_link") - self.goal
+        vec = self.get_vec_gripper_to_goal()
         reward_dist = -np.linalg.norm(vec)
         reward_ctrl = -np.square(action).sum()
         reward = reward_dist + 0.5 * 0.1 * reward_ctrl
@@ -22,8 +23,18 @@ class BlueReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
         done = False
         return ob, reward, done, dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl)
 
+    def get_vec_gripper_to_goal(self):
+        gripper_pos = self.get_gripper_position()
+        vec_gripper_to_goal = self.goal - gripper_pos
+        return vec_gripper_to_goal
+
     def viewer_setup(self):
         self.viewer.cam.trackbodyid = 0
+
+    def do_simulation(action, frame_skip):
+        assert frame_skip > 0
+        for _ in range(frame_skip):
+            self.set_joint_torques(action)
 
     def reward(self, obs, act, obs_next):
         assert obs.ndim == act.ndim == obs_next.ndim
@@ -42,33 +53,30 @@ class BlueReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
         else:
             raise NotImplementedError
 
-    def reset_model(self):
-        qpos = self.init_qpos # + self.np_random.uniform(low=-0.1, high=0.1, size=self.model.nq)
+    def reset(self):
+        self.set_joint_positions(self.init_qpos)
         while True:
             self.goal = np.random.uniform(low=-.2, high=.2, size=3)
-            self.goal = np.array([-.3, -.3, 1])
+            self.goal = np.array([-.3, -.3, 1]) # TODO: Remove this line
             if np.linalg.norm(self.goal) < 2:
                 break
-        qvel = self.init_qvel # + self.np_random.uniform(low=-.005, high=.005, size=self.model.nv)
-        self.set_state(qpos, qvel)
         return self._get_obs()
 
     def _get_obs(self):
-        theta = self.sim.data.qpos.flat
         return np.concatenate([
-            np.cos(theta),
-            np.sin(theta),
-            self.sim.data.qpos.flat,
-            self.sim.data.qvel.flat,
-            self.get_body_com("right_l_finger_tip_link") - self.goal
-        ])
+            self.get_joint_positions(),
+            self.get_joint_velocities(),
+            self.get_gripper_position(),
+            self.get_vec_gripper_to_goal(),
+            ]).reshape(-1)
+
     def log_diagnostics(self, paths, prefix=''):
-        dist = [-path["env_infos"]['reward_dist'] for path in paths]
-        final_dist = [-path["env_infos"]['reward_dist'][-1] for path in paths]
+        dist = [path["env_infos"]['reward_dist'] for path in paths]
+        final_dist = [path["env_infos"]['reward_dist'][-1] for path in paths]
         ctrl_cost = [-path["env_infos"]['reward_ctrl'] for path in paths]
 
-        logger.logkv(prefix + 'AvgDist', np.mean(dist))
-        logger.logkv(prefix + 'AvgFinalDist', np.mean(final_dist))
+        logger.logkv(prefix + 'AvgDistance', np.mean(dist))
+        logger.logkv(prefix + 'AvgFinalDistance', np.mean(final_dist))
         logger.logkv(prefix + 'AvgCtrlCost', np.mean(ctrl_cost))
 
 
