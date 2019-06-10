5f56a3a1720c78e86ba0474e5e937a2d0ad5360e
BorenTsai <boren.tsai@berkeley.edu>
2019-06-10 11:34:12 -0700

    clean jelly env

--- meta_mb/envs/jelly/full_jelly.py
+++ meta_mb/envs/jelly/full_jelly.py
@@ -1,26 +1,57 @@
 import numpy as np
-from gym.envs.mujoco import mujoco_env
-from gym import utils
+#from gym.envs.mujoco import mujoco_env
+#from gym import utils
 import os
+import gym
+from meta_mb.logger import logger
+from gym.envs.mujoco.mujoco_env import MujocoEnv
+from meta_mb.meta_envs.base import MetaEnv
 
-class FullJellyEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+class FullJellyEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
     def __init__(self):
-        utils.EzPickle.__init__(**locals())
-
         xml_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'assets', 'jelly.xml')
-        # Goal still to be specified
-        #self.goal = np.zeros(3)
-
-        mujoco_env.MujocoEnv.__init__(self, xml_file, 2)
+        MujocoEnv.__init__(self, xml_file, 2)
+        gym.utils.EzPickle.__init__(self)
 
     def _get_obs(self):
-        return
+        return np.concatenate([
+            self.sim.data.qpos.flat[1:],
+            self.sim.data.qvel.flat,
+        ])
 
-    def step(self):
-        return
+    def step(self, action):
+        xposbefore = self.sim.data.qpos[0]
+        self.do_simulation(action, self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+        ob = self._get_obs()
+        reward_ctrl = - 0.5 * 0.1 * np.square(action).sum()
+        reward_run = (xposafter - xposbefore) / self.dt
+        reward = reward_ctrl + reward_run
+        done = False
+        return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
+
+    def reward(self, obs, act, obs_next):
+        assert obs.ndim == act.ndim == obs_next.ndim
+        if obs.ndim == 2:
+            assert obs.shape == obs_next.shape and act.shape[0] == obs.shape[0]
+            reward_ctrl = -0.5 * 0.1 * np.sum(np.square(act), axis=1)
+            reward_run = obs_next[:, 8]
+            reward = reward_run + reward_ctrl
+            return np.clip(reward, -1e2, 1e2)
+        elif obs.ndim == 1:
+            assert obs.shape == obs_next.shape
+            reward_ctrl = -0.5 * 0.1 * np.sum(np.square(act))
+            reward_run = obs_next[8]
+            reward = reward_run + reward_ctrl
+            return np.clip(reward, -1e2, 1e2)
+        else:
+            raise NotImplementedError
 
     def reset_model(self):
-        return
+        qpos = self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
 
     def foot_position(self, foot):
         return
