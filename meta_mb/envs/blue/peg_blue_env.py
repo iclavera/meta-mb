4663d3a706bf4556369af85d8569a6252aa6eca5
BorenTsai <boren.tsai@berkeley.edu>
2019-06-26 20:58:05 -0700

    modified reward function of single blue arm peg task

--- meta_mb/envs/blue/peg_blue_env.py
+++ meta_mb/envs/blue/peg_blue_env.py
@@ -8,15 +8,18 @@ from meta_mb.meta_envs.base import RandomEnv
 import mujoco_py
 
 
-class PegFullBlueEnv(RandomEnv, utils.EzPickle):
-    def __init__(self, goal_dist=3e-2):
+class PegArmBlueEnv(RandomEnv, utils.EzPickle):
+    def __init__(self, goal_dist=0.1, log_rand=0, frame_skip=20):
         utils.EzPickle.__init__(**locals())
 
         xml_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'assets', 'blue_right_peg_v2.xml')
 
         self.peg_loc = np.zeros(3)
-        self.peg_table = np.zeros(3)
+        self.reach_goal = np.zeros(3)
+        self.peg_board = np.zeros(3)
         self.goal_dist = goal_dist  # permissible distance from goal
+
+        self.reached = False
 
         RandomEnv.__init__(self, 2, xml_file, 20)
 
@@ -24,17 +27,22 @@ class PegFullBlueEnv(RandomEnv, utils.EzPickle):
     def _get_obs(self):
         return np.concatenate([
             self.sim.data.qpos.flat,
-            self.sim.data.qvel.flat[:-3],
+            self.sim.data.qvel.flat,
             self.get_body_com("peg"),
-            self.peg_location() - self.peg_table
+            self.peg_location() - self.peg_board
         ])
 
     def step(self, action):
         self.do_simulation(action, self.frame_skip)
-
         self.peg_loc = self.peg_location()
-        reward_dist = -self.peg_dist()
-        reward_ctrl = -np.square(action).sum()
+
+        if not self.reached:
+            reward_dist = -self.reach_dist()
+        else:
+            reward_dist = -self.peg_insertion()
+
+        joint_velocities = self.sim.data.qvel
+        reward_ctrl = -np.square(joint_velocities).sum()
         reward = reward_dist + 1.25e-4 * reward_ctrl
 
         observation = self._get_obs()
@@ -46,17 +54,12 @@ class PegFullBlueEnv(RandomEnv, utils.EzPickle):
         qpos = self.init_qpos + self.np_random.uniform(low=-0.01, high=0.01, size=self.model.nq)
         qvel = self.init_qvel + self.np_random.uniform(low=-0.01, high=0.01, size=self.model.nv)
 
-        self.peg_table = self.random_pos()
-        self.sim.model.body_pos[-3] = self.peg_table
-        self.sim.model.body_pos[-2] = np.array([0.092, 0, 0.065])
-        self.sim.model.body_pos[-1] = np.array([0.092, 0, 0])
+        self.peg_board = self.random_pos()
+        self.sim.model.body_pos[-4] = self.peg_board
+        self.sim.model.body_pos[-3] = self.g1()
+        self.sim.model.body_pos[-2] = self.g2()
+        self.sim.model.body_pos[-1] = self.g3()
 
-        qpos[-3:] = self.peg_table + np.array([0.092, 0, 0.040])
-        qpos[-6:-3] = self.peg_table
-
-        qvel[-6:] = 0
-
-        self.set_state(qpos, qvel)
         observation = self._get_obs()
         return observation
 
@@ -65,7 +68,10 @@ class PegFullBlueEnv(RandomEnv, utils.EzPickle):
         if obs.ndim == 2:
             assert obs.shape == obs_next.shape and act.shape[0] == obs.shape[0]
             reward_ctrl = -np.sum(np.square(act), axis=1)
-            reward_dist = -self.peg_dist()
+            if not self.reached:
+                reward_dist = -2 * self.reach_dist()
+            else:
+                reward_dist = -self.peg_insertion()
             reward = reward_dist + 1.25e-4 * reward_ctrl
             return np.clip(reward, -1e2, 1e2)
         elif obs.ndim == 1:
@@ -94,25 +100,39 @@ class PegFullBlueEnv(RandomEnv, utils.EzPickle):
     def peg_orient(self):
         return self.data.get_body_xquat("peg")
 
-    def peg_dist(self):
-        return 2
+    def reach_dist(self):
+        center = self.get_body_com("center")
+        g1 = self.get_body_com("g1")
 
-    def top(self, center):
-        x = center[0] + 0.092
-        y = center[1] 
-        z = center[2] + 0.075
+        dist = euclidean(center, g1)
+        if dist < self.goal_dist:
+            self.reached = True
+        return dist
+
+    def peg_insertion(self):
+        top = self.get_body_com("top")
+        bottom = self.get_body_com("bottom")
+        g2 = self.get_body_com("g2")
+        g3 = self.get_body_com("g3")
+
+        return euclidean(top, g2) + euclidean(bottom, g3)
+
+    def g1(self):
+        x = 0.092
+        y = 0
+        z = 0.75
         return np.array([x, y, z])
 
-    def center(self, center):
-        x = center[0] 
-        y = center[1] + 0.3
-        z = center[2] - 0.55
+    def g2(self):
+        x = 0.092
+        y = 0 
+        z = 0.078
         return np.array([x, y, z])
 
-    def bottom(self, center):
-        x = center[0]
-        y = center[1] + 0.3
-        z = center[2] - 0.7
+    def g3(self):
+        x = 0.092
+        y = 0
+        z = 0
         return np.array([x, y, z])
 
     def viewer_setup(self):
@@ -123,7 +143,7 @@ class PegFullBlueEnv(RandomEnv, utils.EzPickle):
 
 
 if __name__ == "__main__":
-    env = PegFullBlueEnv()
+    env = PegArmBlueEnv()
     while True:
         env.reset()
         for _ in range(500):
