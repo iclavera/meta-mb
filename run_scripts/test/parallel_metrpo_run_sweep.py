c9d239ee5dca95184b79d9db4474e1a91372575e
Ignasi <ignasi78@gmail.com>
2019-07-04 00:02:11 -0700

    run script pabti3

--- run_scripts/test/parallel_metrpo_run_sweep.py
+++ run_scripts/test/parallel_metrpo_run_sweep.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
         'num_rollouts': [1],
 
         'simulation_sleep_frac': [1],
-        'env': ['Hopper', 'Ant'],
+        'env': ['Walker2d', 'HalfCheetah'],
 
         # Problem Conf
 
