0955f9b7d0709bb2e19256aafdd6fd2e4b787985
Ignasi <ignasi78@gmail.com>
2019-07-04 12:28:05 -0700

    pabti0

--- run_scripts/test/parallel_mbppo_run_sweep.py
+++ run_scripts/test/parallel_mbppo_run_sweep.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
 
         'simulation_sleep_frac': [1],
 
-        'env': ['Walker2d', 'Hopper'],
+        'env': ['Walker2d', 'HalfCheetah'],
 
         # Problem Conf
         'algo': ['meppo'],
