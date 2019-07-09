339425d27b975da7967088a5a21090260b85eed6
VioIchigo <violetfuyao@berkeley.edu>
2019-07-09 14:43:23 -0700

    change

--- meta_mb/trainers/sac_edit_trainer.py
+++ meta_mb/trainers/sac_edit_trainer.py
@@ -13,7 +13,16 @@ from pdb import set_trace as st
 from meta_mb.replay_buffers import SimpleReplayBuffer
 from random import randint
 
-
+def format_samples_for_training(samples):
+    obs = samples[0]
+    act = samples[1]
+    next_obs = samples[2]
+    delta_obs = next_obs - obs
+    rewards = samples[3]
+    rewards = rewards[:, None]
+    inputs = np.concatenate((obs, act), axis=-1)
+    outputs = np.concatenate((rewards, delta_obs), axis=-1)
+    return inputs, outputs
 
 class Trainer(object):
     """
@@ -34,61 +43,56 @@ class Trainer(object):
             self,
             algo,
             env,
+            env_name,
             env_sampler,
             env_sample_processor,
-            model_sample_processor,
             dynamics_model,
             policy,
             n_itr,
             rollout_length_params,
-            dynamics_model_max_epochs=200,
-            policy_update_per_iteration=20,
             num_model_rollouts = 1,
-            start_itr=0,
-            task=None,
+            # start_itr=0,
             sess=None,
             n_initial_exploration_steps=1e3,
-            env_num_grad_steps=None,
-            model_num_grad_steps=None,
             env_max_replay_buffer_size=1e6,
-            model_max_replay_buffer_size=4*1e7,
-            speed_up_factor=100,
+            model_max_replay_buffer_size=2e6,
             rollout_batch_size=100,
-            model_train_freq=100,
+            model_reset_freq=1000,
             n_train_repeats=1,
             real_ratio = 1,
-            rollout_length = 1
+            rollout_length = 1,
+            max_model_t = 1e10,
+            model_deterministic = False,
+            epoch_length=1000,
+            model_train_freq=250,
+            restore_path=None,
             ):
         self.algo = algo
+        self.env_name = env_name
         self.env = env
         self.env_sampler = env_sampler
         self.env_sample_processor = env_sample_processor
-        self.model_sample_processor = model_sample_processor
         self.dynamics_model = dynamics_model
         self.baseline = env_sample_processor.baseline
         self.policy = policy
         self.n_itr = n_itr
-        self.start_itr = start_itr
+        self.model_reset_freq = model_reset_freq
+        # self.start_itr = start_itr
         self.num_model_rollouts = num_model_rollouts
-        self.task = task
         self.rollout_length_params = rollout_length_params
         self.n_initial_exploration_steps = n_initial_exploration_steps
         self.env_replay_buffer = SimpleReplayBuffer(self.env, env_max_replay_buffer_size)
         self.model_replay_buffer = SimpleReplayBuffer(self.env, model_max_replay_buffer_size)
-        self.dynamics_model_max_epochs = dynamics_model_max_epochs
-        self.env_num_grad_steps = env_num_grad_steps
         self.rollout_batch_size = rollout_batch_size
         self.rollout_length = rollout_length
-        self.speed_up_factor = speed_up_factor
-        if env_num_grad_steps is None:
-            self.env_num_grad_steps = self.env_sampler.total_samples
-        # if model_num_grad_steps is None:
-        self.model_num_grad_steps = policy_update_per_iteration * self.env_num_grad_steps
-        self.policy_update_per_iteration = policy_update_per_iteration
-        self.model_train_freq = model_train_freq
         self.n_train_repeats = n_train_repeats
         self.real_ratio = real_ratio
-        #     self.model_num_grad_steps = 20 * self.env_sampler.total_samples
+        self.max_model_t = max_model_t
+        self.model_deterministic = model_deterministic
+        self.epoch_length = epoch_length
+        self.model_train_freq = model_train_freq
+        self.restore_path = restore_path
+
 
         if sess is None:
             sess = tf.Session()
@@ -111,100 +115,68 @@ class Trainer(object):
             uninit_vars = [var for var in tf.global_variables() if not sess.run(tf.is_variable_initialized(var))]
             sess.run(tf.variables_initializer(uninit_vars))
             start_time = time.time()
+            self.saver = tf.train.Saver(write_version=tf.train.SaverDef.V2, max_to_keep=10, pad_step_number=True)
 
-            if self.start_itr == 0:
+            # if self.start_itr == 0:
+            if tf.train.get_checkpoint_state(self.restore_path):
+                print("reading stored model from %s", self.restore_path)
+                self.saver.restore(sess, tf.train.latest_checkpoint(self.restore_path))
+                self.start_itr = self.itr.eval() + 1
+
+            else:
+                self.start_itr = 0
                 self.algo._update_target(tau=1.0)
-                if self.n_initial_exploration_steps > 0:
-                    while self.env_replay_buffer._size <= self.n_initial_exploration_steps:
-                        paths = self.env_sampler.obtain_samples(log=True, log_prefix='train-', random=True)
-                        samples_data = self.env_sample_processor.process_samples(paths, log='all', log_prefix='train-')
-                        self.env_replay_buffer.add_samples(samples_data['observations'], samples_data['actions'], samples_data['rewards'],
-                                                            samples_data['dones'], samples_data['next_observations'])
-                        # self.dynamics_model.fit(samples_data['observations'], samples_data['actions'], samples_data['next_observations'],
-                        #                         epochs=self.dynamics_model_max_epochs, verbose=False,
-                        #                         log_tabular=True, prefix='Model-')
+                while self.env_replay_buffer._size < self.n_initial_exploration_steps:
+                    paths = self.env_sampler.obtain_samples(log=True, log_prefix='train-', random=True)
+                    samples_data = self.env_sample_processor.process_samples(paths, log='all', log_prefix='train-')
+                    self.env_replay_buffer.add_samples(samples_data['observations'], samples_data['actions'], samples_data['rewards'],
+                                                        samples_data['dones'], samples_data['next_observations'])
+        
 
             for itr in range(self.start_itr, self.n_itr):
+                self.itr = itr
                 itr_start_time = time.time()
                 logger.log("\n ---------------- Iteration %d ----------------" % itr)
                 logger.log("Sampling set of tasks/goals for this meta-batch...")
-
-                """ -------------------- Sampling --------------------------"""
-                # logger.log("Processing samples...")
-                # time_proc_samples_start = time.time()
-                # samples_data = self.env_sample_processor.process_samples(paths, log='all', log_prefix='train-')
-                # sample_num = samples_data['observations'].shape[0]
-                # self.env_replay_buffer.add_samples(samples_data['observations'], samples_data['actions'], samples_data['rewards'],
-                #                                     samples_data['dones'], samples_data['next_observations'])
-                # proc_samples_time = time.time() - time_proc_samples_start
-                # model_start = time.time()
-                # paths = self.env_sampler.obtain_samples(log=True, log_prefix='eval-', deterministic=True)
-                # _ = self.env_sample_processor.process_samples(paths, log='all', log_prefix='eval-')
-
-                model_fit_start = time.time()
-                # all_samples = self.env_replay_buffer.all_samples()
-                all_samples = self.env_replay_buffer.random_batch(self.env_replay_buffer._size // 3)
-                self.dynamics_model.fit(all_samples['observations'], all_samples['actions'], all_samples['next_observations'],
-                                        epochs=self.dynamics_model_max_epochs, verbose=False,
-                                        log_tabular=True, prefix='Model-')
-                logger.logkv('environment buffer size', self.env_replay_buffer._size)
-                logger.logkv('fit_model_time', time.time() - model_fit_start)
-                for _ in range(self.env_sampler.max_path_length // self.model_train_freq):
+                time_step = 0
+                for _ in range(self.epoch_length // self.model_train_freq):
                     for _ in range(self.rollout_length):
+                        max_t = self.max_model_t if itr > 100 else 1e10
+                        model_metrics = self._train_model(batch_size=256, epochs=300000, max_grad_updates=1000000, holdout_ratio=0.2, max_t=max_t)
                         random_states = self.env_replay_buffer.random_batch_simple(int(self.rollout_batch_size))['observations']
                         actions_from_policy = self.policy.get_actions(random_states)[0]
-                        predictions = self.dynamics_model.predict(random_states, actions_from_policy)
-                        rewards = self.algo.training_environment.reward(random_states, actions_from_policy, predictions)
-                        terminals = np.array([False]).repeat(random_states.shape[0])
-                        self.model_replay_buffer.add_samples(random_states, actions_from_policy, rewards, terminals, predictions)
+                        next_obs, rewards, term, info = self.step(random_states, actions_from_policy, deterministic=self.model_deterministic)
+                        term = term.reshape((-1))
+                        # rewards = rewards.reshape((-1))
+                        rewards = self.env.reward(random_states, actions_from_policy, next_obs)
+                        self.model_replay_buffer.add_samples(random_states, actions_from_policy, rewards, term, next_obs)
                     self.set_rollout_length(itr)
-                logger.logkv('rollout length', self.rollout_length)
 
-                logger.log("Obtaining samples...")
-                time_env_sampling_start = time.time()
+                    for _ in range(self.model_train_freq):
+                        time_step += 1
+                        for _ in range(self.n_train_repeats):
+                            batch_size = 256
+                            env_batch_size = int(batch_size*self.real_ratio)
+                            model_batch_size = batch_size - env_batch_size
+                            env_batch = self.env_replay_buffer.random_batch(env_batch_size)
+                            model_batch = self.model_replay_buffer.random_batch(int(model_batch_size))
+                            keys = env_batch.keys()
+                            batch = {k: np.concatenate((env_batch[k], model_batch[k]), axis=0) for k in keys}
+                            self.algo.do_training(itr * 1000 + time_step, batch)
                 paths = self.env_sampler.obtain_samples(log=True, log_prefix='train-')
-                sampling_time = time.time() - time_env_sampling_start
-
-                """ ----------------- Processing Samples ---------------------"""
-                logger.log("Processing samples...")
-                time_proc_samples_start = time.time()
                 samples_data = self.env_sample_processor.process_samples(paths, log='all', log_prefix='train-')
-                sample_num = samples_data['observations'].shape[0]
                 self.env_replay_buffer.add_samples(samples_data['observations'], samples_data['actions'], samples_data['rewards'],
-                                                    samples_data['dones'], samples_data['next_observations'])
-                proc_samples_time = time.time() - time_proc_samples_start
-                model_start = time.time()
+                                                        samples_data['dones'], samples_data['next_observations'])
                 paths = self.env_sampler.obtain_samples(log=True, log_prefix='eval-', deterministic=True)
                 _ = self.env_sample_processor.process_samples(paths, log='all', log_prefix='eval-')
 
-                for _ in range(self.n_train_repeats * self.env_sampler.max_path_length):
-                    batch_size = 256
-                    env_batch_size = int(batch_size*self.real_ratio)
-                    model_batch_size = batch_size - env_batch_size
-                    env_batch = self.env_replay_buffer.random_batch(env_batch_size)
-                    model_batch = self.model_replay_buffer.random_batch(int(model_batch_size))
-                    keys = env_batch.keys()
-                    batch = {k: np.concatenate((env_batch[k], model_batch[k]), axis=0) for k in keys}
-                    self.algo.do_training(itr, batch)
-
-
+                # self.saver.save(sess, os.path.join(self.restore_path, ''))
                 self.log_diagnostics(paths, prefix='train-')
-
-
-                train_actor_start = time.time()
 
                 """ ------------------- Logging Stuff --------------------------"""
                 logger.logkv('Itr', itr)
+                logger.logkv('rollout_length', self.rollout_length)
                 logger.logkv('n_timesteps', self.env_sampler.total_timesteps_sampled)
-                # logger.logkv('train_critic_time', critic_end - time_optimization_step_start)
-                # logger.logkv('train_actor_time', sum(actor_time))
-                # logger.logkv('predict_time', train_actor_start - predict_start)
-#
-                # logger.logkv('Time-Optimization', time.time() - time_optimization_step_start)
-                # logger.logkv('Time-SampleProc', np.sum(proc_samples_time))
-                # logger.logkv('Time-Sampling', sampling_time)
-
-                # logger.logkv('Time', time.time() - start_time)
                 logger.logkv('ItrTime', time.time() - itr_start_time)
 
                 logger.log("Saving snapshot...")
@@ -213,11 +185,102 @@ class Trainer(object):
                 logger.log("Saved")
 
                 logger.dumpkvs()
-                if itr == 0:
-                    sess.graph.finalize()
+                # if itr == 0:
+                #     sess.graph.finalize()
 
         logger.log("Training finished")
         self.sess.close()
+
+
+    def termination_fn(self, obs, act, next_obs):
+        assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
+        if self.env_name == 'AntEnv':
+            x = next_obs[:, 0]
+            not_done = 	np.isfinite(next_obs).all(axis=-1) * (x >= 0.2) * (x <= 1.0)
+            done = ~not_done
+        elif self.env_name == 'HalfCheetahEnv' or 'Walker2dEnv':
+            done = np.array([False]).repeat(obs.shape[0])
+        elif self.env_name == 'HopperEnv':
+            height = next_obs[:, 0]
+            angle = next_obs[:, 1]
+            not_done =  np.isfinite(next_obs).all(axis=-1) \
+                        * np.abs(next_obs[:,1:] < 100).all(axis=-1) \
+                        * (height > .7) \
+                        * (np.abs(angle) < .2)
+
+            done = ~not_done
+        else:
+            print("Error")
+            return
+        done = done[:,None]
+        return done
+
+    def _get_logprob(self, x, means, variances):
+
+        k = x.shape[-1]
+        log_prob = -1/2 * (k * np.log(2*np.pi) + np.log(variances).sum(-1) + (np.power(x-means, 2)/variances).sum(-1))
+
+        ## [ batch_size ]
+        prob = np.exp(log_prob).sum(0)
+        log_prob = np.log(prob)
+        stds = np.std(means,0).mean(-1)
+
+        return log_prob, stds
+
+
+    def _train_model(self, **kwargs):
+        env_samples = self.env_replay_buffer.all_samples()
+        train_inputs, train_outputs = format_samples_for_training(env_samples)
+        model_metrics = self.dynamics_model.train(train_inputs, train_outputs, **kwargs)
+        return model_metrics
+
+    def step(self, obs, act, deterministic=False):
+        assert len(obs.shape) == len(act.shape)
+        if len(obs.shape) == 1:
+            obs = obs[None]
+            act = act[None]
+            return_single = True
+        else:
+            return_single = False
+
+        inputs = np.concatenate((obs, act), axis=-1)
+        ensemble_model_means, ensemble_model_vars = self.dynamics_model.predict(inputs, factored=True)
+        ensemble_model_means[:,:,1:] += obs
+        ensemble_model_stds = np.sqrt(ensemble_model_vars)
+
+        if deterministic:
+            ensemble_samples = ensemble_model_means
+        else:
+            ensemble_samples = ensemble_model_means + np.random.normal(size=ensemble_model_means.shape) * ensemble_model_stds
+
+        #### choose one model from ensemble
+        num_models, batch_size, _ = ensemble_model_means.shape
+        model_inds = self.dynamics_model.random_inds(batch_size)
+        batch_inds = np.arange(0, batch_size)
+        samples = ensemble_samples[model_inds, batch_inds]
+        model_means = ensemble_model_means[model_inds, batch_inds]
+        model_stds = ensemble_model_stds[model_inds, batch_inds]
+        ####
+
+        log_prob, dev = self._get_logprob(samples, ensemble_model_means, ensemble_model_vars)
+
+        rewards, next_obs = samples[:,:1], samples[:,1:]
+        terminals = self.termination_fn(obs, act, next_obs)
+        ## add terminal reward and stds
+        batch_size = model_means.shape[0]
+        return_means = np.concatenate((model_means[:,:1], terminals, model_means[:,1:]), axis=-1)
+        return_stds = np.concatenate((model_stds[:,:1], np.zeros((batch_size,1)), model_stds[:,1:]), axis=-1)
+
+        if return_single:
+            next_obs = next_obs[0]
+            return_means = return_means[0]
+            return_stds = return_stds[0]
+            rewards = rewards[0]
+            terminals = terminals[0]
+
+        info = {'mean': return_means, 'std': return_stds, 'log_prob': log_prob, 'dev': dev}
+        return next_obs, rewards, terminals, info
+
 
     def set_rollout_length(self, itr):
         min_epoch, max_epoch, min_length, max_length = self.rollout_length_params
@@ -229,9 +292,7 @@ class Trainer(object):
             y = dx * (max_length - min_length) + min_length
 
         self.rollout_length = int(y)
-        # print('[ Model Length ] Epoch: {} (min: {}, max: {}) | Length: {} (min: {} , max: {})'.format(
-        #     itr, min_epoch, max_epoch, self._rollout_length, min_length, max_length
-        # ))
+
 
     def get_itr_snapshot(self, itr):
         """
@@ -244,24 +305,3 @@ class Trainer(object):
         self.env.log_diagnostics(paths, prefix)
         self.policy.log_diagnostics(paths, prefix)
         self.baseline.log_diagnostics(paths, prefix)
-
-        # feed_dict = self._get_feed_dict(iteration, batch)
-        # # TODO(hartikainen): We need to unwrap self._diagnostics_ops from its
-        # # tensorflow `_DictWrapper`.
-        # diagnostics = self._session.run({**self._diagnostics_ops}, feed_dict)
-        #
-        # diagnostics.update(OrderedDict([
-        #     (f'policy/{key}', value)
-        #     for key, value in
-        #     self._policy.get_diagnostics(flatten_input_structure({
-        #         name: batch['observations'][name]
-        #         for name in self._policy.observation_keys
-        #     })).items()
-        # ]))
-        #
-        # if self._plotter:
-        #     self._plotter.draw()
-        #
-        # return diagnostics
-        #
-        # return diagnostics
--- run_scripts/sac_edit_run_sweep.py
+++ run_scripts/sac_edit_run_sweep.py
@@ -3,9 +3,11 @@ import json
 import tensorflow as tf
 import numpy as np
 INSTANCE_TYPE = 'c4.2xlarge'
-EXP_NAME = 'MBPO3'
+EXP_NAME = 'MBPO2'
+import sys
+sys.path.append('/home/vioichigo/meta-mb/tf_mbpo/mbpo/handful-of-trails')
 
-
+from pdb import set_trace as st
 from meta_mb.algos.sac_edit import SAC_MB
 from experiment_utils.run_sweep import run_sweep
 from meta_mb.utils.utils import set_seed, ClassEncoder
@@ -21,6 +23,24 @@ from meta_mb.baselines.linear_baseline import LinearFeatureBaseline
 from meta_mb.dynamics.mlp_dynamics_ensemble import MLPDynamicsEnsemble
 from meta_mb.dynamics.probabilistic_mlp_dynamics_ensemble import ProbMLPDynamicsEnsemble
 
+from dmbrl.modeling.layers import FC
+from dmbrl.modeling.models import BNN
+
+save_model_dir = 'home/vioichigo/Desktop/meta-mb/Saved_Model/' + EXP_NAME + '/'
+
+def construct_model(obs_dim=11, act_dim=3, rew_dim=1, num_networks=7, num_elites=5):
+	print('[ BNN ] Observation dim {} | Action dim: {}'.format(obs_dim, act_dim))
+	params = {'name': 'BNN', 'num_networks': num_networks, 'num_elites': num_elites}
+	model = BNN(params)
+
+	model.add(FC(200, input_dim=obs_dim+act_dim, activation="swish", weight_decay=0.000025))
+	model.add(FC(200, activation="swish", weight_decay=0.00005))
+	model.add(FC(200, activation="swish", weight_decay=0.000075))
+	model.add(FC(200, activation="swish", weight_decay=0.000075))
+	model.add(FC(obs_dim+rew_dim, weight_decay=0.0001))
+	model.finalize(tf.train.AdamOptimizer, {"learning_rate": 0.001})
+	return model
+
 
 def run_experiment(**kwargs):
     exp_dir = os.getcwd() + '/data/' + EXP_NAME
@@ -34,7 +54,7 @@ def run_experiment(**kwargs):
 
         # Instantiate classes
         set_seed(kwargs['seed'])
-
+ 
         baseline = kwargs['baseline']()
 
         env = normalize(kwargs['env']())
@@ -75,24 +95,14 @@ def run_experiment(**kwargs):
             positive_adv=kwargs['positive_adv'],
         )
 
-        dynamics_model = ProbMLPDynamicsEnsemble('dynamics-ensemble',
-                                                env=env,
-                                                num_models=kwargs['num_models'],
-                                                hidden_sizes=kwargs['dynamics_hidden_sizes'],
-                                                hidden_nonlinearity=kwargs['dyanmics_hidden_nonlinearity'],
-                                                output_nonlinearity=kwargs['dyanmics_output_nonlinearity'],
-                                                batch_size=kwargs['dynamics_batch_size'],
-                                                learning_rate=kwargs['model_learning_rate'],
-                                                buffer_size=kwargs['dynamics_buffer_size'],
-                                                )
+        dynamics_model = construct_model(env.observation_space.shape[0], 
+                                        env.action_space.shape[0], 
+                                        rew_dim=1, 
+                                        num_networks=kwargs['num_models'], 
+                                        num_elites=kwargs['num_elites'],
+                                        )
 
-        dynamics_sample_processor = ModelSampleProcessor(
-            baseline=baseline,
-            discount=kwargs['discount'],
-            gae_lambda=kwargs['gae_lambda'],
-            normalize_adv=kwargs['normalize_adv'],
-            positive_adv=kwargs['positive_adv'],
-        )
+
 
         algo = SAC_MB(
             policy=policy,
@@ -103,7 +113,6 @@ def run_experiment(**kwargs):
             Qs=Qs,
             Q_targets=Q_targets,
             reward_scale=kwargs['reward_scale'],
-            sampler_batch_size=kwargs['sampler_batch_size'],
             num_actions_per_next_observation=kwargs['num_actions_per_next_observation'],
             prediction_type=kwargs['prediction_type'],
             T=kwargs['T'],
@@ -112,25 +121,26 @@ def run_experiment(**kwargs):
         trainer = Trainer(
             algo=algo,
             env=env,
+            env_name=str(kwargs['env']),
             env_sampler=env_sampler,
             env_sample_processor=env_sample_processor,
-            model_sample_processor = dynamics_sample_processor,
             dynamics_model=dynamics_model,
             policy=policy,
             n_itr=kwargs['n_itr'],
-            dynamics_model_max_epochs=kwargs['dynamics_model_max_epochs'],
-            policy_update_per_iteration=kwargs['policy_update_per_iteration'],
             num_model_rollouts=kwargs['num_model_rollouts'],
             sess=sess,
             n_initial_exploration_steps=kwargs['n_initial_exploration_steps'],
             env_max_replay_buffer_size=kwargs['env_replay_buffer_max_size'],
             model_max_replay_buffer_size=kwargs['model_replay_buffer_max_size'],
-            speed_up_factor=kwargs['speed_up_factor'],
             rollout_length_params=kwargs['rollout_length_params'],
             rollout_batch_size=kwargs['rollout_batch_size'],
             model_train_freq=kwargs['model_train_freq'],
+            model_reset_freq=kwargs['model_reset_freq'],
             n_train_repeats=kwargs['n_train_repeats'],
             real_ratio=kwargs['real_ratio'],
+            max_model_t=kwargs['max_model_t'],
+            epoch_length=kwargs['epoch_length'],
+            restore_path=save_model_dir+kwargs['restore_path'],
         )
 
         trainer.train()
@@ -140,46 +150,37 @@ def run_experiment(**kwargs):
 if __name__ == '__main__':
     sweep_params = {
         'algo': ['sac'],
-        'seed': [1, 22],
+        'seed': [11],
         'baseline': [LinearFeatureBaseline],
         'env': [HalfCheetahEnv],
-
         # Policy
         'policy_hidden_sizes': [(256, 256)],
         'policy_learn_std': [True],
         'policy_output_nonlinearity': [None],
-        'policy_update_per_iteration': [40],
-        'speed_up_factor': [100],
-        'num_model_rollouts': [20],
+        'num_model_rollouts': [1],
 
         # Env Sampling
         'num_rollouts': [1],
         'n_parallel': [1],
 
         # replay_buffer
-        'env_replay_buffer_max_size': [1e5],
+        'env_replay_buffer_max_size': [1e6],
         'model_replay_buffer_max_size': [2e6],
 
         # Problem Conf
-        'n_itr': [400],
-        'max_path_length': [1000],
+        'n_itr': [3000],
+        'n_train_repeats': [8],
+        'max_path_length': [101],
         'discount': [0.99],
         'gae_lambda': [1.],
         'normalize_adv': [True],
         'positive_adv': [False],
         'learning_rate': [3e-4],
         'reward_scale': [1],
-        'sampler_batch_size': [256],
 
         # Dynamics Model
         'num_models': [7],
-        'model_learning_rate': [1e-3],
-        'dynamics_hidden_sizes': [(200, 200, 200, 200)],
-        'dyanmics_hidden_nonlinearity': ['relu'],
-        'dyanmics_output_nonlinearity': [None],
-        'dynamics_batch_size': [512],
-        'dynamics_buffer_size': [10000],
-        'dynamics_model_max_epochs': [50],
+        'num_elites': [5],
 
         'num_actions_per_next_observation': [3],
         'prediction_type':['mean'],
@@ -187,11 +188,12 @@ if __name__ == '__main__':
         'n_initial_exploration_steps': [5e3],
         'rollout_length_params': [[20, 150, 1, 1]],
         'model_reset_freq': [1000],
-        'model_train_freq': [250],
+        'model_train_freq': [25],
         'rollout_batch_size': [100e3],
-        'n_train_repeats': [8],
-        'real_ratio': [0.05]
-
+        'real_ratio': [0.05],
+        'max_model_t': [1e10],
+        'epoch_length': [100],
+        'restore_path': ['']
 
         }
 
