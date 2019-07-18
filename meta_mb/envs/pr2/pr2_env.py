e7c0b6694cc1f179d294c2aaca8a601632db1df8
BorenTsai <boren.tsai@berkeley.edu>
2019-07-17 17:18:50 -0700

    pr2 reacher env

--- meta_mb/envs/pr2/pr2_env.py
+++ meta_mb/envs/pr2/pr2_env.py
@@ -7,7 +7,14 @@ import os
 
 
 class PR2ReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
-    def __init__(self):
+    def __init__(self,
+                 max_torques=[3] * 7,
+                 vel_penalty=1.25e-2,
+                 torque_penalty=1.25e-1):
+        self.max_torques = np.array(max_torques)
+        self.vel_penalty = -vel_penalty
+        self.torque_penalty = -torque_penalty
+
         self.goal = np.zeros(3)
         self.goal2 = np.array([-0.30982005, 0.71146246, 0.51908543,
                                -0.14216614, 0.78684261, 0.56139753,
@@ -21,32 +28,41 @@ class PR2ReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
         self.init_qpos = np.array([3.85207921e-01, -1.41945343e-01, 1.64343706e+00, -1.51601210e+00,
                                    1.31405443e+00, -1.54883181e+00, 1.43069760e-01])
         self.init_qvel = np.zeros(7)
-        self.offset_0 = np.array([ 0.8571748,  -0.04651993,  0.76642218])
-        self.offset_1 = np.array([ 0.88839121, -0.07360844,  0.68504259])
-        self.offset_2 = np.array([ 0.69829279, -0.08945529,  0.76459628])
-        MujocoEnv.__init__(self, xml_file, 2)
+        self.alpha = 10e-3
+        self.offset_0 = np.array([0.02, -0.025,  0.05])
+        self.offset_1 = np.array([0.02, -0.025, -0.05])
+        self.offset_2 = np.array([0.02,  0.05,   0.0])
+        MujocoEnv.__init__(self, xml_file, 4)
         gym.utils.EzPickle.__init__(self)
 
     def step(self, action):
         self.do_simulation(action, self.frame_skip)
-        vec = self.get_body_com("l_gripper_r_finger_tip_link") - self.goal
-        reward_dist = -np.linalg.norm(vec)
-        reward_ctrl = -np.square(action).sum()
-        reward = reward_dist + 0.5 * 0.1 * reward_ctrl
         ob = self._get_obs()
+        vec = self.ee_position - self.goal2
+        norm = np.linalg.norm(vec)
+        reward_dist = -(np.square(norm) + np.log(np.square(norm) + self.alpha))
+        reward_vel = self.vel_penalty * np.square(np.linalg.norm(ob[7:14]))
+        reward_ctrl = self.torque_penalty * np.square(np.linalg.norm(action))
+        reward = reward_dist + reward_vel + reward_ctrl
         done = False
-        return ob, reward, done, dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl)
+        return ob, reward, done, dict(dist=norm, reward_vel=reward_vel, reward_ctrl=reward_ctrl)
 
     def viewer_setup(self):
         self.viewer.cam.trackbodyid = 0
+
+    def tf_reward(self, obs, act, obs_next):
+        return "wut"
 
     def reward(self, obs, act, obs_next):
         assert obs.ndim == act.ndim == obs_next.ndim
         if obs.ndim == 2:
             assert obs.shape == obs_next.shape and act.shape[0] == obs.shape[0]
-            reward_ctrl = -0.5 * 0.1 * np.sum(np.square(act), axis=1)
-            reward_dist = -np.linalg.norm(obs_next[:,-3:], axis=1)
-            reward = reward_dist + reward_ctrl
+            vec = obs_next[:, -9:] - self.goal2
+            norm = np.linalg.norm(vec)
+            reward_dist = -(np.square(norm) + np.log(np.square(norm) + self.alpha))
+            reward_vel = self.vel_penalty * np.square(np.linalg.norm(obs_next[:, 7:14]))
+            reward_ctrl = self.torque_penalty * np.square(np.linalg.norm(act))
+            reward = reward_dist + reward_vel + reward_ctrl
             return np.clip(reward, -1e3, 1e3)
         elif obs.ndim == 1:
             assert obs.shape == obs_next.shape
@@ -58,14 +74,17 @@ class PR2ReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
             raise NotImplementedError
 
     def reset_model(self):
-        #qpos = np.array([3.85207921e-01, -1.41945343e-01, 1.64343706e+00, -1.51601210e+00,
-        #                               1.31405443e+00, -1.54883181e+00, 1.43069760e-01]) # + self.np_random.uniform(low=-0.1, high=0.1, size=self.model.nq)
-        qpos = np.zeros(7)
-        self.sim.model.body_pos[-1] = self.goal2[:3] + self.offset_0
-        self.sim.model.body_pos[-2] = self.goal2[3:6] + self.offset_1
-        self.sim.model.body_pos[-3] = self.goal2[6:] + self.offset_2
+        qpos = self.init_qpos
+        #self.sim.model.body_pos[-3] = self.goal2[:3]
+        #self.sim.model.body_pos[-2] = self.goal2[3:6]
+        #self.sim.model.body_pos[-1] = self.goal2[6:]
         while True:
-            self.goal = np.random.uniform(low=-.2, high=.2, size=3)
+            self.goal = np.random.uniform(low=0.2, high=.6, size=3)
+            self.goal2 = np.concatenate([
+                self.goal + self.offset_0,
+                self.goal + self.offset_1,
+                self.goal + self.offset_2
+            ])
             #self.goal = np.array([-.3, -.3, 1])
             if np.linalg.norm(self.goal) < 2:
                 break
@@ -81,13 +100,16 @@ class PR2ReacherEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
         ])
 
     def log_diagnostics(self, paths, prefix=''):
-        dist = [-path["env_infos"]['reward_dist'] for path in paths]
-        final_dist = [-path["env_infos"]['reward_dist'][-1] for path in paths]
+        dist = [-path["env_infos"]['dist'] for path in paths]
+        final_dist = [-path["env_infos"]['dist'][-1] for path in paths]
         ctrl_cost = [-path["env_infos"]['reward_ctrl'] for path in paths]
+        #vel_cost = [-path["env_infos"]['reward_vel'] for path in paths]
 
         logger.logkv(prefix + 'AvgDist', np.mean(dist))
         logger.logkv(prefix + 'AvgFinalDist', np.mean(final_dist))
         logger.logkv(prefix + 'AvgCtrlCost', np.mean(ctrl_cost))
+        #logger.logkv(prefix + 'AvgVelCost', np.mean(vel_cost))
+
 
     @property
     def ee_position(self):
@@ -105,6 +127,7 @@ if __name__ == "__main__":
         print(env.sim.data.get_site_xpos("p2"))
         print(env.sim.data.get_site_xpos("p3"))
         offset = env.ee_position - env.init_grip
+        print(offset)
         for _ in range(1000):
             env.step(env.action_space.sample())
             env.render()
