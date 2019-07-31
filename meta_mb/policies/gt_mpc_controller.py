fix loss sign; learns on reacher, swing up ip

meta_mb/policies/gt_mpc_controller.py
@@ -193,22 +193,25 @@ class GTMPCController(Serializable):
         print(returns)
 
         # do gradient steps
-        returns_array = []  # (num_opt_iters,)
+        # returns_array = []  # (num_opt_iters,)
         for itr in range(self.num_opt_iters):
-            grad_s, grad_a = self.planner.do_gradient_steps(s_array_stacked=s_array, a_array_stacked=a_array)
-            delta_s = self.optimizer_s.compute_delta_var(grad_s)
-            delta_s[0, :] = np.zeros((self.action_space_dims,))
-            s_array += delta_s
-            a_array += self.optimizer_a.compute_delta_var(grad_a)
-            a_array = np.clip(a_array, self.env.action_space.low, self.env.action_space.high)
+            # Set a rolling average here
+            for _ in range(250):
+                grad_s, grad_a = self.planner.do_gradient_steps(s_array_stacked=s_array, a_array_stacked=a_array)
+                delta_s = self.optimizer_s.compute_delta_var(grad_s)
+                delta_s[0, :] = np.zeros((self.obs_space_dims,))
+                s_array -= delta_s
+                a_array -= self.optimizer_a.compute_delta_var(grad_a)
+                a_array = np.clip(a_array, self.env.action_space.low, self.env.action_space.high)
 
             # report performance
             _, returns = self._run_open_loop(a_array)
-            returns_array.append(returns)
-            if itr % 200 == 0:
-                print(returns)
+            # returns_array.append(returns)
+            logger.logkv('Itr', itr)
+            logger.logkv('Return', returns)
+            logger.dumpkvs()
 
-        return returns_array[-1]
+        return returns[None]
 
     def get_rollouts_opt_act(self, deterministic=False, plot_first_rollout=False):
         """

meta_mb/samplers/vectorized_env_executor.py
@@ -691,7 +691,7 @@ def collocation_worker(remote, parent_remote, env_pickle, eps,
         grad_s = np.zeros((obs_dim, obs_dim))
         for idx in range(obs_dim):  # compute grad[:, idx]
             s[idx] += eps
-            new_s_next, _, _, _ = step_wrapper(s, a)
+            new_s_next = step_wrapper(s, a)
             s[idx] -= eps
             grad_s[idx] = (new_s_next - old_s_next) / eps
 
@@ -707,7 +707,7 @@ def collocation_worker(remote, parent_remote, env_pickle, eps,
         grad_a = np.zeros((obs_dim, act_dim))
         for idx in range(act_dim): # compute grad[:, idx]
             a[idx] += eps
-            new_s_next, _, _, _ = step_wrapper(s, a)
+            new_s_next = step_wrapper(s, a)
             a[idx] -= eps
             grad_a[:, idx] = (new_s_next - old_s_next) / eps
         return grad_a
@@ -736,8 +736,8 @@ def collocation_worker(remote, parent_remote, env_pickle, eps,
 
             for t in range(n_envs):
                 s, a = s_array[t], a_array[t]
-                _grad_s = discount**t * dr_ds(s, a) + lmbda * (s - f_array[t-1])
-                _grad_a = discount**t * dr_da(s, a)
+                _grad_s = -discount**t * dr_ds(s, a) + lmbda * (s - f_array[t-1])
+                _grad_a = -discount**t * dr_da(s, a)
                 if t != n_envs-1:
                     s_next = s_array[t+1]
                     _grad_s += -lmbda * np.matmul(df_ds(s, a).T, s_next - f_array[t])