b4e5a9a4e399e1edf77f5c1370504016de5277aa
Yunzhi Zhang <zzyunzhi@gmail.com>
2019-08-11 11:47:22 -0700

    ilqr

--- meta_mb/policies/gt_ilqr_controller.py
+++ meta_mb/policies/gt_ilqr_controller.py
@@ -13,62 +13,30 @@ class GTiLQRController(Serializable):
             env,
             dynamics_model,
             eps,
-            num_rollouts=None,
-            reward_model=None,
-            discount=1,
-            lmbda=1,
-            method_str='opt_policy',
-            n_parallel=1,
-            dyn_pred_str='rand',
-            initializer_str='uniform',
-            reg_coef=1,
-            reg_str=None,
-            n_candidates=1024,
-            horizon=10,
-            max_path_length=200,
-            num_cem_iters=8,
-            num_opt_iters=8,
-            num_collocation_iters=500,
+            num_rollouts,
+            discount,
+            n_parallel,
+            initializer_str,
+            n_candidates,
+            horizon,
+            max_path_length,
             num_ddp_iters=100,
-            mu=1e-3,
-            persistency=0.99,
-            opt_learning_rate=1e-3,
-            clip_norm=-1,
             percent_elites=0.1,
-            use_reward_model=False,
             alpha=0.25,
-            num_particles=1,
             verbose=True,
     ):
         Serializable.quick_init(self, locals())
-        self.dynamics_model = dynamics_model
-        self.reward_model = reward_model
         self.discount = discount
-        self.method_str = method_str
-        self.dyn_pred_str = dyn_pred_str
         self.initializer_str = initializer_str
-        self.reg_coef = reg_coef
-        assert 0 <= self.reg_coef <= 1
-        self.reg_str = reg_str
         self.n_candidates = n_candidates
         self.horizon = horizon
         self.max_path_length = max_path_length
-        self.num_cem_iters = num_cem_iters
-        self.num_opt_iters = num_opt_iters
-        self.num_collocation_iters = num_collocation_iters
         self.num_ddp_iters = num_ddp_iters
-        self.persistency= persistency
-        self.opt_learning_rate = opt_learning_rate
         self.eps = eps
         self.num_envs = num_rollouts
-        self.percent_elites = percent_elites
         self.num_elites = max(int(percent_elites * n_candidates), 1)
         self.env = env
-        self.use_reward_model = use_reward_model
         self.alpha = alpha
-        self.num_particles = num_particles
-        self.clip_norm = clip_norm
-        self.lmbda = lmbda
 
         self.unwrapped_env = env
         while hasattr(self.unwrapped_env, '_wrapped_env'):
@@ -115,31 +83,33 @@ class GTiLQRController(Serializable):
         ilqr_itr_counter = 0
         while ilqr_itr_counter < self.num_ddp_iters:
             optimized_action, backward_accept, forward_accept, planner_returns_log, reward_array = self.planner.update_x_u_for_one_step(obs=obs)
-            if not backward_accept:
-                # reset
-                logger.log(f'perturb u array, restart ilqr iterations')
-                self.planner.perturb_u_array()
-                ilqr_itr_counter = 0
-            else:
-                # log
-                if ilqr_itr_counter % 10 == 0:
-                    # logger.log(f'stats for x, max = {np.max(self.planner.x_array)}, min = {np.min(self.planner.x_array)}, mean {np.mean(self.planner.x_array)}')
-                    logger.log(f'stats for u, max = {np.max(self.planner.u_array)}, min = {np.min(self.planner.u_array)}, mean {np.mean(self.planner.u_array)}')
+            # if not backward_accept:
+            #     pass
+            #     # reset
+            #     # logger.log(f'reset u array, restart ilqr iterations')
+            #     # self.planner.perturb_u_array()
+            #     # # self.planner.reset_u_array()
+            #     # ilqr_itr_counter = 0
+            # else:
+            # log
+            if ilqr_itr_counter % 10 == 0:
+                # logger.log(f'stats for x, max = {np.max(self.planner.x_array)}, min = {np.min(self.planner.x_array)}, mean {np.mean(self.planner.x_array)}')
+                logger.log(f'stats for u, max = {np.max(self.planner.u_array)}, min = {np.min(self.planner.u_array)}, mean {np.mean(self.planner.u_array)}')
 
-                if forward_accept:
-                    old_returns, new_returns, diff = planner_returns_log
-                    if reward_array is not None:
-                        reward_array = np.reshape(reward_array, (-1, 5))
-                        for row in reward_array:
-                            logger.log(row)
-                    logger.logkv('Itr', ilqr_itr_counter)
-                    logger.logkv('PlannerPrevReturn', old_returns)
-                    logger.logkv('PlannerReturn', new_returns)
-                    logger.logkv('ExpectedDiff', diff)
-                    logger.logkv('ActualDiff', new_returns - old_returns)
-                    logger.dumpkvs()
+            if backward_accept and forward_accept:
+                old_returns, new_returns, diff = planner_returns_log
+                if reward_array is not None:
+                    reward_array = np.reshape(reward_array, (-1, 5))
+                    for row in reward_array:
+                        logger.log(row)
+                logger.logkv('Itr', ilqr_itr_counter)
+                logger.logkv('PlannerPrevReturn', old_returns)
+                logger.logkv('PlannerReturn', new_returns)
+                logger.logkv('ExpectedDiff', diff)
+                logger.logkv('ActualDiff', new_returns - old_returns)
+                logger.dumpkvs()
 
-                ilqr_itr_counter += 1
+            ilqr_itr_counter += 1
 
         # shift
         u_new = None  # self._generate_new_u()
@@ -152,7 +122,7 @@ class GTiLQRController(Serializable):
             init_u_array = self._init_u_array_cem()
         elif self.initializer_str == 'uniform':
             init_u_array = np.random.uniform(low=self.env.action_space.low, high=self.env.action_space.high,
-                                                     size=(self.horizon, self.action_space_dims))
+                                             size=(self.horizon, self.action_space_dims))
         elif self.initializer_str == 'zeros':
             init_u_array = np.random.normal(scale=0.1, size=(self.horizon, self.action_space_dims))
             init_u_array = np.clip(init_u_array, a_min=self.env.action_space.low, a_max=self.env.action_space.high)
